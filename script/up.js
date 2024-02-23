cb72b6dde7f5348636a6b76c8a696ba7afce3c23
Maxim Zavidiy <zavidiy1998@mail.ru>
2024-02-23 11:51:03 +0200

    first commit

---
+++ script/up.js
@@ -0,0 +1,9 @@
+function upMap(){
+    const map = document.querySelector('.map');
+    map.classList.add('active');
+}
+
+function closeMap(){
+    const map = document.querySelector('.map');
+    map.classList.remove('active');
+}
