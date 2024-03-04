8565b08e0e482fea0e396c5c5fbe9414f7e8a166
Maxim Zavidiy <zavidiy1998@mail.ru>
2024-03-04 18:04:21 +0200

    v4

--- script/up.js
+++ script/up.js
@@ -7,3 +7,15 @@ function closeMap(){
     const map = document.querySelector('.map');
     map.classList.remove('active');
 }
+
+function showRight(){
+    document.addEventListener('click', ()=>{
+        window.location.href = '/help/home.html';
+    })
+}
+
+function showLeft(){
+    document.addEventListener('click', ()=>{
+        window.location.href = '/training/news.html';
+    })
+}
